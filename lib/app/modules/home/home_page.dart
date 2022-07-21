b33ffe9f04c8e3b83302c77cca437944c8fa4ae2
Filipe Haniel <filipe.hanielloper@gmail.com>
2022-07-21 10:00:34 -0300

    testando carregamento de pacote

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:teste_button/teste_button.dart';
 
 import 'home_controller.dart';
 
 class HomePage extends GetView<HomeController> {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: Column(
+          children: const [
+            PackButton(
+              text: 'Testando',
+              height: 70,
+              width: 200,
+            ),
+            TesteTextField(obs: true),
+            CardTest(text: 'testando'),
+          ],
+        ),
+      ),
     );
   }
 }
